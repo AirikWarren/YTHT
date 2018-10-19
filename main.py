db7c8cc65768f2bdf42657389e900cb925d68072
airikWarren <airikwarren@gmail.com>
2018-10-19 03:23:24 -0700

    committed main.py and requirements.txt to repo

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pyperclip
+import webbrowser
+import keyboard
+
+def main():
+        keyboard.add_hotkey('ctrl+c', paste_hooktube_url_to_clipboard)
+        keyboard.wait('ctrl+q')
+        print("quitting. . .")
+        
+def paste_hooktube_url_to_clipboard():
+        hooktube_url = convert_yt_url(pyperclip.paste())
+        pyperclip.copy(hooktube_url)
+
+def convert_yt_url(url):
+        if 'youtube' in url:
+                return url.replace('youtube', 'hooktube')
+        elif 'youtu.be' in url:
+                return url.replace('youtu.be', 'hooktube')
+        else:
+                return url
+
+if __name__ == '__main__':
+        main()
